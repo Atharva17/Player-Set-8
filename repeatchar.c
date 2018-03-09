bb1e6b4264a1cae1841f772f584c97371efee28b
Harshini A <harshinidurai98@gmail.com>
2018-03-09 16:30:16 +0530

    Create repeatchar.c

---
+++ repeatchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char s[50];
+    int i,j,count=0;
+    scanf("%s",s);
+    for(i=0;s[i]!='\0';i++)
+      for(j=i+1;s[j]!='\0';j++)
+        if(s[i]==s[j])
+          count++;
+     if(count>0)
+     printf("yes");
+     else
+     printf("no");
+      return 0;
+}
